3941cb4030c4a7a7b8b13846e5a2654254817add
Погодин <pogodinvitalyv@gmail.com>
2023-11-03 12:34:08 +0300

    composer.json + basic routes + index.php initial setup

---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    '/home' => function () {
+        echo '<h2>Its home here</h2>';
+    },
+    '/movies' => function () {
+        echo '<h2>Movies page</h2>';
+    },
+];
--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$routes = require_once __DIR__.'/config/routes.php';
+
+$uri = $_SERVER['REQUEST_URI'];
+$routes[$uri]();
